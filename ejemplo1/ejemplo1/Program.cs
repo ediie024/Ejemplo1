0629496d0958c2a5657800c65d17ae56a7f70bd4
Eduardo Collantes <64392831+ediie024@users.noreply.github.com>
2020-08-30 15:54:15 -0500

    nuevo

---
+++ ejemplo1/ejemplo1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ejemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("G!");
+            Console.WriteLine("I");
+            Console.WriteLine("T");
+              Console.WriteLine("h!");
+                        Console.WriteLine("u");
+                        Console.WriteLine("b");
+        }
+    }
+}
